d74b06606e9bc42327d32313fa2fa48ad442a584
joy43 <ssjoy43@gmail.com>
2023-08-19 09:33:24 +0600

    first commit

---
+++ script/area calculate.js
@@ -0,0 +1,75 @@
+//---------------tringle calculate --------------
+function calculateTringleArea(){
+   
+  const baseField=document.getElementById('tinangel-base');
+  const baseValueText=baseField.value;
+  const base=parseFloat(baseValueText);
+  // console.log(base);
+
+// height
+const heightField=document.getElementById('tingle-height');
+const heightValueText=heightField.value;
+const height=parseFloat(heightValueText);
+// console.log(height);
+const area=0.5*base*height;
+console.log(area);
+// show tringle area
+const areaSpan=document.getElementById('triangle-Area');
+areaSpan.innerText=area;
+}
+
+// -------------------Rectangle calculate------------------
+function calculateRectangleArea(){
+  // width
+  const withField=document.getElementById('rectangle-width')
+  const widthValueText=withField.value;
+  const width=parseFloat(widthValueText)
+  // console.log(width);
+
+  // length
+  const lengthField=document.getElementById('rectangle-length');
+  const lengthValueText=lengthField.value;
+  const length=parseFloat(lengthValueText);
+  // console.log(height);
+  const Rectanglearea=width*length;
+  console.log(Rectanglearea);
+  // show tringle area
+  const RectangleareaSpan=document.getElementById('Rectsnangle-Area');
+  RectangleareaSpan.innerText=Rectanglearea;
+
+}
+
+
+//-------- -------reusable function------- ----------- 
+
+function calculateParallelogramArea(){
+const base=getInputValue('parallrlogram-base');
+// console.log(value);
+
+// height
+const height=document.getElementById('parallelogram-height');
+// console.log(height);
+const area=base*height;
+console.log(area);
+setElementInnerText('parallelogram-area',area);
+
+
+
+
+}
+// resable get input value field
+function getInputValue(fieldId){
+  const inputField=document.getElementById(fieldId);
+  inputValueText=inputField.value;
+  const value=parseFloat(inputValueText);
+  return value;
+
+}
+// resuable set span,p,div etc
+function setElementInnerText(elementId,area){
+  const element=document.getElementById(elementId)
+  element.innerText=area;
+  
+  
+}
+
